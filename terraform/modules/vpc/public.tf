3ec537a9b8c3e5908c0c2a68796b89e289540034
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-06-04 16:01:29 +0100

    Adding ip address after validating it was false positive

--- terraform/modules/vpc/public.tf
+++ terraform/modules/vpc/public.tf
@@ -25,6 +25,6 @@ resource "aws_route_table_association" "public" {
 
 resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
-  destination_cidr_block = ""
+  destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
