5693462a472d3ae5bc6f4c21cc5a3682c856fd0f
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-06-09 04:16:46 +0100

    Adding security group rules necessary for recieving and send prometheus traffic

--- terraform/modules/prometheus/security_groups.tf
+++ terraform/modules/prometheus/security_groups.tf
@@ -8,3 +8,30 @@ resource "aws_security_group" "web" {
     create_before_destroy = true
   }
 }
+
+resource "aws_security_group_rule" "allow_egress_https" {
+  type              = "egress"
+  to_port           = 443
+  protocol          = "tcp"
+  prefix_list_ids   = [var.vpc.s3_prefix_list_id]
+  from_port         = 443
+  security_group_id = aws_security_group.web.id
+}
+
+resource "aws_security_group_rule" "allow_ingress_prom" {
+  type                      = "ingress"
+  to_port                   = 9090
+  protocol                  = "tcp"
+  from_port                 = 9090
+  security_group_id         = aws_security_group.web.id
+  source_security_group_id  = var.lb_security_group_id
+}
+
+resource "aws_security_group_rule" "allow_egress_prom" {
+  type              = "egress"
+  to_port           = 9090
+  protocol          = "tcp"
+  from_port         = 9090
+  security_group_id = aws_security_group.web.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
