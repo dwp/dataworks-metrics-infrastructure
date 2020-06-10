64386cea56a37f78097338d903c01486af06552f
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-06-10 07:46:06 +0100

    Tightened prometheus security group

--- terraform/modules/prometheus/security_groups.tf
+++ terraform/modules/prometheus/security_groups.tf
@@ -9,15 +9,6 @@ resource "aws_security_group" "web" {
   }
 }
 
-resource "aws_security_group_rule" "allow_egress_https" {
-  type              = "egress"
-  to_port           = 443
-  protocol          = "tcp"
-  prefix_list_ids   = [var.vpc.s3_prefix_list_id]
-  from_port         = 443
-  security_group_id = aws_security_group.web.id
-}
-
 resource "aws_security_group_rule" "allow_ingress_prom" {
   type                     = "ingress"
   to_port                  = 9090
