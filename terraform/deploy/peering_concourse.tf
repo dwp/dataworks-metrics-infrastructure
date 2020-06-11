ed549954ed0981ff57554fa3719d6ccc2af906c6
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-06-11 15:33:41 +0100

    Updating to plural for clarity

--- terraform/deploy/peering_concourse.tf
+++ terraform/deploy/peering_concourse.tf
@@ -1,6 +1,6 @@
 resource "aws_route" "route" {
-  count                     = length(data.terraform_remote_state.aws_concourse.outputs.route_table)
-  route_table_id            = data.terraform_remote_state.aws_concourse.outputs.route_table[count.index].id
+  count                     = length(data.terraform_remote_state.aws_concourse.outputs.route_tables)
+  route_table_id            = data.terraform_remote_state.aws_concourse.outputs.route_tables[count.index].id
   destination_cidr_block    = local.cidr_block[local.environment].mon-master-vpc
   vpc_peering_connection_id = module.vpc.outputs.prometheus_pcx
 }
