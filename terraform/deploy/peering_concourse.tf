952d0f11b9cd7bc9476ec43cc95a321856734ae1
iw-gtadam <61688288+iw-gtadam@users.noreply.github.com>
2020-06-11 16:08:35 +0100

    Vpc peering (#11)

---
+++ terraform/deploy/peering_concourse.tf
@@ -0,0 +1,7 @@
+resource "aws_route" "route" {
+  count                     = length(data.terraform_remote_state.aws_concourse.outputs.route_tables)
+  route_table_id            = data.terraform_remote_state.aws_concourse.outputs.route_tables[count.index].id
+  destination_cidr_block    = local.cidr_block[local.environment].mon-master-vpc
+  vpc_peering_connection_id = module.vpc.outputs.prometheus_pcx
+}
+  
