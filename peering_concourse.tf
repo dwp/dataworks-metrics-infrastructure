78aa518696dcc99ec5fade7adc757e215ffd0fba
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-06-16 23:44:19 +0100

    Using newly exported VPC ID

--- peering_concourse.tf
+++ peering_concourse.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "peering" {
   count       = local.roles[0] == "master" ? 1 : 0
-  peer_vpc_id = data.terraform_remote_state.aws_concourse.outputs.route_tables[0].vpc_id
+  peer_vpc_id = data.terraform_remote_state.aws_concourse.outputs.vpc_id
   vpc_id      = module.vpc.outputs.vpc_ids[1]
   auto_accept = true
   tags        = merge(local.tags, { Name = "prometheus_pcx" })
