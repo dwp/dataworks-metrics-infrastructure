2e0ad8a0c3828a8c4822900aead2cf6e2e3e13a2
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-06-22 18:53:17 +0100

    Splitting out s3 operations into separate file and creating a monitoring s3 bucket

---
+++ s3.tf
@@ -0,0 +1,86 @@
+resource "random_id" "monitoring_bucket" {
+  count       = local.is_management_env ? 1 : 0
+  byte_length = 16
+}
+
+resource "aws_s3_bucket" "monitoring" {
+  count  = local.is_management_env ? 1 : 0
+  bucket = random_id.monitoring_bucket[local.primary_role_index].hex
+  acl    = "private"
+  tags   = local.tags
+  versioning {
+    enabled = false
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "monitoring" {
+  count  = local.is_management_env ? 1 : 0
+  bucket = aws_s3_bucket.monitoring[local.primary_role_index].id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+data "aws_iam_policy_document" "monitoring_bucket_enforce_https" {
+  count = local.is_management_env ? 1 : 0
+  statement {
+    sid     = "BlockHTTP"
+    effect  = "Deny"
+    actions = ["*"]
+
+    resources = [
+      aws_s3_bucket.monitoring[local.primary_role_index].arn,
+      "${aws_s3_bucket.monitoring[local.primary_role_index].arn}/*",
+    ]
+
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+
+    condition {
+      test     = "Bool"
+      values   = ["false"]
+      variable = "aws:SecureTransport"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "monitoring" {
+  count  = local.is_management_env ? 1 : 0
+  bucket = aws_s3_bucket.monitoring[local.primary_role_index].id
+  policy = local.is_management_env ? data.aws_iam_policy_document.monitoring_bucket_enforce_https[local.primary_role_index].json : "{}"
+}
+
+data template_file "prometheus" {
+  count    = length(local.roles)
+  template = file("${path.module}/config/prometheus/prometheus-${local.roles[count.index]}.tpl")
+  vars = {
+    parent_domain_name = var.parent_domain_name
+  }
+}
+
+data template_file "thanos" {
+  template = file("${path.module}/config/thanos/bucket.tpl")
+  vars = {
+    metrics_bucket = local.is_management_env ? aws_s3_bucket.monitoring[local.primary_role_index].id : data.terraform_remote_state.management_dmi.outputs.monitoring_bucket.id
+    s3_endpoint    = "s3-${var.region}.amazonaws.com"
+  }
+}
+
+resource "aws_s3_bucket_object" "prometheus" {
+  count      = length(local.roles)
+  bucket     = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${var.name}/prometheus/prometheus-${local.roles[count.index]}.yml"
+  content    = data.template_file.prometheus[count.index].rendered
+  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.cmk_arn : data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
+
+resource "aws_s3_bucket_object" "thanos" {
+  bucket     = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${var.name}/thanos/bucket.yml"
+  content    = data.template_file.thanos.rendered
+  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.cmk_arn : data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
