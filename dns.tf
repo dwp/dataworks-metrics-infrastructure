e13cb6cd03736d5aa6bf1e7ea58eab04133578a9
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-06-23 13:51:02 +0100

    Adding grafana DNS entry

--- dns.tf
+++ dns.tf
@@ -42,6 +42,21 @@ resource "aws_route53_record" "thanos_loadbalancer" {
   provider = aws.management_dns
 }
 
+resource "aws_route53_record" "grafana_loadbalancer" {
+  count   = local.is_management_env ? 1 : 0
+  name    = "grafana.${local.fqdn}"
+  type    = "A"
+  zone_id = data.terraform_remote_state.management_dns.outputs.dataworks_zone.id
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_lb.monitoring[0].dns_name
+    zone_id                = aws_lb.monitoring[0].zone_id
+  }
+
+  provider = aws.management_dns
+}
+
 resource "aws_acm_certificate" "monitoring" {
   count             = local.is_management_env ? 1 : 0
   domain_name       = "*.${local.fqdn}"
