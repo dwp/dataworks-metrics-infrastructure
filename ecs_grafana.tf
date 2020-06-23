7d04e9ebc7a44af67dc4f6d634d012157814811a
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-06-23 14:00:21 +0100

    Adding grafana container

---
+++ ecs_grafana.tf
@@ -0,0 +1,93 @@
+resource "aws_ecs_task_definition" "grafana" {
+  count                    = local.is_management_env ? 1 : 0
+  family                   = "grafana"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "4096"
+  task_role_arn            = aws_iam_role.prometheus[count.index].arn
+  execution_role_arn       = local.is_management_env ? data.terraform_remote_state.management.outputs.ecs_task_execution_role.arn : data.terraform_remote_state.common.outputs.ecs_task_execution_role.arn
+
+  container_definitions = <<DEFINITION
+[
+  {
+    "cpu": ${var.fargate_cpu},
+    "image": "grafana-temp:temp",
+    "memory": ${var.fargate_memory},
+    "name": "grafana",
+    "networkMode": "awsvpc",
+    "user": "0:0",
+    "portMappings": [
+      {
+        "containerPort": 3000,
+        "hostPort": 3000
+      }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "${data.terraform_remote_state.management.outputs.ecs_cluster_main_log_group.name}",
+        "awslogs-region": "${data.aws_region.current.name}",
+        "awslogs-stream-prefix": "grafana"
+      }
+    },
+    "placementStrategy": [
+      {
+        "field": "attribute:ecs.availability-zone",
+        "type": "spread"
+      }
+    ],
+    "environment": [
+      {
+        "name": "GRAFANA_CONFIG_S3_BUCKET",
+        "value": "${local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id}"
+      },
+      {
+        "name": "GRAFANA_CONFIG_S3_PREFIX",
+        "value": "${var.name}/grafana"
+      }
+    ]
+  }
+]
+DEFINITION
+}
+
+resource "aws_ecs_service" "grafana" {
+  count            = local.is_management_env ? 1 : 0
+  name             = "grafana"
+  cluster          = data.terraform_remote_state.management.outputs.ecs_cluster_main.id
+  task_definition  = aws_ecs_task_definition.grafana[local.primary_role_index].arn
+  platform_version = "1.4.0"
+  desired_count    = 1
+  launch_type      = "FARGATE"
+
+  network_configuration {
+    security_groups = [aws_security_group.prometheus[local.primary_role_index].id]
+    subnets         = module.vpc.outputs.private_subnets[local.primary_role_index]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.grafana[local.primary_role_index].arn
+    container_name   = "grafana"
+    container_port   = 3000
+  }
+
+  service_registries {
+    registry_arn   = aws_service_discovery_service.grafana[local.primary_role_index].arn
+    container_name = "grafana"
+  }
+}
+
+resource "aws_service_discovery_service" "grafana" {
+  count = local.is_management_env ? 1 : 0
+  name  = "grafana"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.monitoring.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+  }
+}
