29d01fd9a8781ac2367eda7393be5a30e797f269
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-06-23 01:26:11 +0100

    Adding thanos query record

--- dns.tf
+++ dns.tf
@@ -27,6 +27,21 @@ resource "aws_route53_record" "monitoring_loadbalancer" {
   provider = aws.management_dns
 }
 
+resource "aws_route53_record" "thanos_loadbalancer" {
+  count   = local.is_management_env ? 1 : 0
+  name    = "thanos.${local.fqdn}"
+  type    = "A"
+  zone_id = data.terraform_remote_state.management_dns.outputs.dataworks_zone.id
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_lb.monitoring[0].dns_name
+    zone_id                = aws_lb.monitoring[0].zone_id
+  }
+
+  provider = aws.management_dns
+}
+
 resource "aws_acm_certificate" "monitoring" {
   count             = local.is_management_env ? 1 : 0
   domain_name       = join(".", [local.roles[count.index], local.fqdn])
