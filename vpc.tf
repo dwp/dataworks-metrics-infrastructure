556d2a828caceb23f17932dcb48902e09c88d532
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-06-28 13:32:58 +0100

    Amending reference to route table

--- vpc.tf
+++ vpc.tf
@@ -55,7 +55,7 @@ resource "aws_route" "public" {
 
 resource "aws_route_table_association" "public" {
   count          = local.is_management_env ? local.zone_count : 0
-  route_table_id = aws_route.public[count.index].id
+  route_table_id = aws_route_table.public[count.index].id
   subnet_id      = aws_subnet.public[count.index].id
 }
 
