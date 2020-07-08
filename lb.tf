2f1c5ee2965b7156a40fdce948f99f322e1fdaa3
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-07-09 00:58:11 +0100

    Reverting changes

--- lb.tf
+++ lb.tf
@@ -8,13 +8,13 @@ resource "aws_lb" "monitoring" {
   tags               = merge(local.tags, { Name = "${var.name}-lb" })
 }
 
-resource "aws_lb_listener" "grafana" {
+resource "aws_lb_listener" "monitoring" {
   count             = local.is_management_env ? 1 : 0
   load_balancer_arn = aws_lb.monitoring[0].arn
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-  certificate_arn   = aws_acm_certificate.grafana[0].arn
+  certificate_arn   = aws_acm_certificate.monitoring[0].arn
 
   default_action {
     type = "fixed-response"
@@ -27,23 +27,26 @@ resource "aws_lb_listener" "grafana" {
   }
 }
 
-resource "aws_lb_listener" "thanos" {
-  count             = local.is_management_env ? 1 : 0
-  load_balancer_arn = aws_lb.monitoring[0].arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-  certificate_arn   = aws_acm_certificate.thanos[0].arn
+resource "aws_lb_target_group" "prometheus" {
+  count       = local.is_management_env ? 1 : 0
+  name        = "${var.primary}-${var.name}-http"
+  port        = var.prometheus_port
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.outputs.vpcs[local.primary_role_index].id
+  target_type = "ip"
 
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "FORBIDDEN"
-      status_code  = "403"
-    }
+  health_check {
+    port    = var.prometheus_port
+    path    = "/-/healthy"
+    matcher = "200"
   }
+
+  stickiness {
+    enabled = true
+    type    = "lb_cookie"
+  }
+
+  tags = merge(local.tags, { Name = "prometheus" })
 }
 
 resource "aws_lb_target_group" "thanos" {
@@ -65,19 +68,19 @@ resource "aws_lb_target_group" "thanos" {
     type    = "lb_cookie"
   }
 
-  tags = merge(local.tags, { Name = "thanos" })
+  tags = merge(local.tags, { Name = "prometheus" })
 }
 
 resource "aws_lb_target_group" "grafana" {
   count       = local.is_management_env ? 1 : 0
   name        = "grafana-http"
-  port        = var.grafana_port
+  port        = 3000
   protocol    = "HTTP"
   vpc_id      = module.vpc.outputs.vpcs[local.primary_role_index].id
   target_type = "ip"
 
   health_check {
-    port    = var.grafana_port
+    port    = 3000
     path    = "/api/health"
     matcher = "200"
   }
@@ -87,12 +90,27 @@ resource "aws_lb_target_group" "grafana" {
     type    = "lb_cookie"
   }
 
-  tags = merge(local.tags, { Name = "grafana" })
+  tags = merge(local.tags, { Name = "prometheus" })
+}
+
+resource "aws_lb_listener_rule" "prometheus" {
+  count        = local.is_management_env ? 1 : 0
+  listener_arn = aws_lb_listener.monitoring[local.primary_role_index].arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.prometheus[local.primary_role_index].arn
+  }
+
+  condition {
+    field  = "host-header"
+    values = [aws_route53_record.monitoring_loadbalancer[local.primary_role_index].fqdn]
+  }
 }
 
 resource "aws_lb_listener_rule" "thanos" {
   count        = local.is_management_env ? 1 : 0
-  listener_arn = aws_lb_listener.thanos[local.primary_role_index].arn
+  listener_arn = aws_lb_listener.monitoring[local.primary_role_index].arn
 
   action {
     type             = "forward"
@@ -107,7 +125,7 @@ resource "aws_lb_listener_rule" "thanos" {
 
 resource "aws_lb_listener_rule" "grafana" {
   count        = local.is_management_env ? 1 : 0
-  listener_arn = aws_lb_listener.grafana[local.primary_role_index].arn
+  listener_arn = aws_lb_listener.monitoring[local.primary_role_index].arn
 
   action {
     type             = "forward"
