d8d347611e24f349cc7d174444d2e44e1606f6e3
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-07-08 19:39:18 +0100

    Moving port numbers to secrets manager

--- bootstrap_terraform.py
+++ bootstrap_terraform.py
@@ -34,6 +34,7 @@ def main():
 
     config_data = yaml.load(parameter['Parameter']['Value'], Loader=yaml.FullLoader)
     config_data['roles'] = json.loads(response['SecretBinary'])[os.getenv('TF_WORKSPACE', 'development')]
+    config_data['ports'] = json.loads(response['SecretBinary'])["ports"]
 
     with open('modules/vpc/vpc.tf.j2') as in_template:
         template = jinja2.Template(in_template.read())
