ce8b7aa71340ac2dfbfe282c11ff2b4930c677e6
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2020-12-09 13:47:30 +0000

    add count

--- grafana_iam.tf
+++ grafana_iam.tf
@@ -95,7 +95,7 @@ data "aws_iam_policy_document" "grafana_read_secret" {
       "secretsmanager:GetSecretValue",
     ]
     resources = [
-      "${local.is_management_env ? data.aws_secretsmanager_secret.monitoring_secret.arn : 0}",
+      data.aws_secretsmanager_secret.monitoring_secret.arn,
     ]
   }
 }
