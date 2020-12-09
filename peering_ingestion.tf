fa76a2604cdc6fdfb3e5b9bb1811087fb9418f64
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-12-09 14:39:23 +0000

    rm ingestion_prometheus_secondary
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- peering_ingestion.tf
+++ peering_ingestion.tf
@@ -6,12 +6,12 @@ resource "aws_vpc_peering_connection" "ingestion" {
   tags        = merge(local.tags, { Name = var.name })
 }
 
-resource "aws_route" "ingestion_prometheus_secondary" {
-  count                     = local.is_management_env ? 0 : 1
-  route_table_id            = data.terraform_remote_state.aws_ingestion.outputs.emr_route_table.id
-  destination_cidr_block    = local.cidr_block[local.environment].mon-slave-vpc
-  vpc_peering_connection_id = aws_vpc_peering_connection.ingestion[0].id
-}
+# resource "aws_route" "ingestion_prometheus_secondary" {
+#   count                     = local.is_management_env ? 0 : 1
+#   route_table_id            = data.terraform_remote_state.aws_ingestion.outputs.emr_route_table.id
+#   destination_cidr_block    = local.cidr_block[local.environment].mon-slave-vpc
+#   vpc_peering_connection_id = aws_vpc_peering_connection.ingestion[0].id
+# }
 
 resource "aws_route" "prometheus_secondary_ingestion" {
   count                     = local.is_management_env ? 0 : local.zone_count
