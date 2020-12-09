95e7b51d05753d1ac79ea716a23e01c33f5f9e41
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2020-12-09 13:52:55 +0000

    check against mgmt

--- grafana_iam.tf
+++ grafana_iam.tf
@@ -95,7 +95,7 @@ data "aws_iam_policy_document" "grafana_read_secret" {
       "secretsmanager:GetSecretValue",
     ]
     resources = [
-      data.aws_secretsmanager_secret.monitoring_secret.arn,
+      "${local.is_management_env ? data.aws_secretsmanager_secret.monitoring_secret[0].arn : 0}",
     ]
   }
 }
