21e152fb88a10fee596e131ee5df0e724afd5f59
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-12-10 15:08:08 +0000

    Add S3 object for dash board (#112)
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- grafana_s3.tf
+++ grafana_s3.tf
@@ -34,7 +34,7 @@ data template_file "analytical_emr_dashboard" {
 }
 
 data template_file "concourse_dashboard" {
-  template = file("${path.module}/config/grafana/provisioning/dashboards/concourse.json")
+  template = file("${path.module}/config/grafana/provisioning/dashboards/concourse_dashboard.json")
 }
 
 resource "aws_s3_bucket_object" "grafana" {
@@ -87,6 +87,15 @@ resource "aws_s3_bucket_object" "analytical_emr_dashboard" {
   bucket     = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
   key        = "${var.name}/grafana/provisioning/dashboards/analytical_emr_dashboard.json"
   content    = data.template_file.analytical_emr_dashboard.rendered
+  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.cmk_arn : data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+  tags       = merge(local.tags, { Name = var.name })
+}
+
+resource "aws_s3_bucket_object" "concourse_dashboard" {
+  count      = local.is_management_env ? 1 : 0
+  bucket     = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${var.name}/grafana/provisioning/dashboards/concourse_dashboard.json"
+  content    = data.template_file.concourse_dashboard.rendered
   kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.cmk_arn : data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
   tags       = merge(local.tags, { Name = var.name })
 }
