41311f0da40389556e33f17e6c7cd0f85055c762
Mark Stevens <mark.stevens@infinitywork.com>
2020-12-14 15:56:55 +0000

    added load balancer listener rule and security rule for thanos store
    Signed-off-by: Mark Stevens <mark.stevens@infinitywork.com>

--- common_lb.tf
+++ common_lb.tf
@@ -285,6 +285,21 @@ resource "aws_lb_listener_rule" "outofband" {
   }
 }
 
+resource "aws_lb_listener_rule" "thanos_store" {
+  count        = local.is_management_env ? 1 : 0
+  listener_arn = aws_lb_listener.monitoring[local.primary_role_index].arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.thanos_store[local.primary_role_index].arn
+  }
+
+  condition {
+    field  = "host-header"
+    values = [aws_route53_record.thanos_store_loadbalancer[local.primary_role_index].fqdn]
+  }
+}
+
 resource "aws_security_group" "monitoring" {
   count  = local.is_management_env ? 1 : 0
   vpc_id = module.vpc.outputs.vpcs[local.primary_role_index].id
@@ -359,4 +374,15 @@ resource "aws_security_group_rule" "allow_egress_outofband" {
   from_port                = var.prometheus_port
   security_group_id        = aws_security_group.monitoring[local.primary_role_index].id
   source_security_group_id = aws_security_group.outofband[local.primary_role_index].id
+}
+
+resource "aws_security_group_rule" "allow_egress_thanos_store" {
+  count                    = local.is_management_env ? 1 : 0
+  description              = "Allow loadbalancer to access thanos store http endpoint"
+  type                     = "egress"
+  to_port                  = var.thanos_port_http
+  protocol                 = "tcp"
+  from_port                = var.thanos_port_http
+  security_group_id        = aws_security_group.monitoring[local.primary_role_index].id
+  source_security_group_id = aws_security_group.thanos_query[local.primary_role_index].id
 }
