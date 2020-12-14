526e3a7e24f8b15e4e49538084dd4a2758a2aa1b
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2020-12-14 17:42:26 +0000

    remove duplicates
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- peering_master.tf
+++ peering_master.tf
@@ -86,63 +86,6 @@ resource "aws_security_group_rule" "thanos_query_allow_egress_thanos_sidecar" {
   source_security_group_id = aws_security_group.prometheus.id
 }
 
-resource "aws_vpc_peering_connection" "prometheus" {
-  peer_owner_id = lookup(local.account, lookup(local.slave_peerings, local.environment))
-  peer_vpc_id   = local.is_management_env ? module.vpc.outputs.vpcs[0].id : data.terraform_remote_state.management_dmi.outputs.vpcs[0].id
-  vpc_id        = module.vpc.outputs.vpcs[local.secondary_role_index].id
-  tags          = merge(local.tags, { Name = var.name })
-}
-
-resource "aws_route" "management_prometheus_secondary_prometheus_primary" {
-  count                     = local.is_management_env ? local.zone_count : 0
-  route_table_id            = module.vpc.outputs.private_route_tables[local.secondary_role_index][count.index]
-  destination_cidr_block    = local.cidr_block_mon_master_vpc[0]
-  vpc_peering_connection_id = aws_vpc_peering_connection.prometheus.id
-}
-
-resource "aws_route" "non_management_prometheus_secondary_prometheus_primary" {
-  count                     = local.is_management_env ? 0 : local.zone_count
-  route_table_id            = module.vpc.outputs.private_route_tables[local.secondary_role_index][count.index]
-  destination_cidr_block    = data.terraform_remote_state.management_dmi.outputs.vpcs[0].cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.prometheus.id
-}
-
-resource "aws_route" "management_prometheus_primary_prometheus_secondary" {
-  count                     = local.is_management_env ? local.zone_count : 0
-  route_table_id            = module.vpc.outputs.private_route_tables[0][count.index]
-  destination_cidr_block    = local.cidr_block[local.environment].mon-slave-vpc
-  vpc_peering_connection_id = aws_vpc_peering_connection.prometheus.id
-}
-
-resource "aws_route" "non_management_prometheus_primary_prometheus_secondary" {
-  provider                  = aws.dmi_management
-  count                     = local.is_management_env ? 0 : local.zone_count
-  route_table_id            = data.terraform_remote_state.management_dmi.outputs.private_route_tables[0][count.index]
-  destination_cidr_block    = local.cidr_block[local.environment].mon-slave-vpc
-  vpc_peering_connection_id = aws_vpc_peering_connection.prometheus.id
-}
-
-resource "aws_security_group_rule" "prometheus_secondary_allow_ingress_prometheus_primary" {
-  description              = "Allow thanos store node to access prometheus"
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = var.prometheus_port
-  to_port                  = var.prometheus_port
-  security_group_id        = aws_security_group.prometheus.id
-  source_security_group_id = local.is_management_env ? aws_security_group.thanos_store[0].id : data.terraform_remote_state.management_dmi.outputs.thanos_security_group
-}
-
-resource "aws_security_group_rule" "prometheus_primary_allow_egress_prometheus_secondary" {
-  provider                 = aws.dmi_management
-  description              = "Allow thanos store node to access prometheus"
-  type                     = "egress"
-  protocol                 = "tcp"
-  from_port                = var.prometheus_port
-  to_port                  = var.prometheus_port
-  security_group_id        = local.is_management_env ? aws_security_group.thanos_store[0].id : data.terraform_remote_state.management_dmi.outputs.thanos_security_group
-  source_security_group_id = aws_security_group.prometheus.id
-}
-
 resource "aws_security_group_rule" "thanos_sidecar_allow_ingress_thanos_store" {
   description              = "Allow thanos store node to access thanos sidecar"
   type                     = "ingress"
