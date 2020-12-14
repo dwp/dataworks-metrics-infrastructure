08d00677431401f2cc9200a85c5c055917f2ecf6
Levan K <37900502+TheDogfather17@users.noreply.github.com>
2020-12-14 15:27:57 +0000

    Added thanos store terraform files (#118)
    * Added thanos store terraform files
    
    Signed-off-by: Mark Stevens <mark.stevens@infinitywork.com>
    
    * Updated thanos store config file to reference query file
    
    Signed-off-by: Mark Stevens <mark.stevens@infinitywork.com>
    
    * added thanos store load balancer target group
    
    Signed-off-by: Mark Stevens <mark.stevens@infinitywork.com>
    
    Co-authored-by: Mark Stevens <mark.stevens@infinitywork.com>

---
+++ thanos_store_sg.tf
@@ -0,0 +1,44 @@
+resource "aws_security_group" "thanos_store" {
+  count       = local.is_management_env ? 1 : 0
+  name        = "thanos-store"
+  description = "Rules necesary for pulling container image and accessing other thanos instances"
+  vpc_id      = module.vpc.outputs.vpcs[local.primary_role_index].id
+  tags        = merge(local.tags, { Name = "thanos-store" })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "allow_loadbalancer_ingress_thanos_store_http" {
+  count                    = local.is_management_env ? 1 : 0
+  description              = "Allows loadbalancer to access thanos user interface"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = var.thanos_port_http
+  to_port                  = var.thanos_port_http
+  security_group_id        = aws_security_group.thanos_store[0].id
+  source_security_group_id = aws_security_group.monitoring[0].id
+}
+
+resource "aws_security_group_rule" "allow_grafana_ingress_thanos_store_http" {
+  count                    = local.is_management_env ? 1 : 0
+  description              = "Allows grafana to access thanos store api"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = var.thanos_port_http
+  to_port                  = var.thanos_port_http
+  security_group_id        = aws_security_group.thanos_store[0].id
+  source_security_group_id = aws_security_group.grafana[0].id
+}
+
+resource "aws_security_group_rule" "allow_thanos_ruler_ingress_thanos_store_http" {
+  count                    = local.is_management_env ? 1 : 0
+  description              = "Allows thanos ruler to access thanos store"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = var.thanos_port_http
+  to_port                  = var.thanos_port_http
+  security_group_id        = aws_security_group.thanos_store[0].id
+  source_security_group_id = aws_security_group.thanos_ruler[0].id
+}
