dde29e971f5a59742165e95970e8e5530f76013c
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-12-16 10:24:38 +0000

    Rm store lb (#142)
    * use only GRPC port
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * remove store's loadbalancer
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * removed some more thanos store load balancer resources
    
    Signed-off-by: Mark Stevens <mark.stevens@infinitywork.com>
    
    Co-authored-by: Mark Stevens <mark.stevens@infinitywork.com>

--- common_dns.tf
+++ common_dns.tf
@@ -96,20 +96,6 @@ resource "aws_route53_record" "outofband_loadbalancer" {
   }
 }
 
-resource "aws_route53_record" "thanos_store_loadbalancer" {
-  provider = aws.management_dns
-  count    = local.is_management_env ? 1 : 0
-  name     = "thanos-store.${local.fqdn}"
-  type     = "A"
-  zone_id  = data.terraform_remote_state.management_dns.outputs.dataworks_zone.id
-
-  alias {
-    evaluate_target_health = false
-    name                   = aws_lb.monitoring[0].dns_name
-    zone_id                = aws_lb.monitoring[0].zone_id
-  }
-}
-
 resource "aws_acm_certificate" "monitoring" {
   count                     = local.is_management_env ? 1 : 0
   domain_name               = local.fqdn
