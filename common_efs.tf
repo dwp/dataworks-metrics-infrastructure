3f112061c8e5b61f16970c658bbf00e752e9d24e
Mark Stevens <Mark814690@gmail.com>
2020-12-17 11:03:34 +0000

    Recreated Efs volume for prometheus (#151)
    * readded efs resources
    
    Signed-off-by: Mark Stevens <mark.stevens@infinitywork.com>
    
    * added zfs volume to prometheus cluster
    
    Signed-off-by: Mark Stevens <mark.stevens@infinitywork.com>
    
    Co-authored-by: Mark Stevens <mark.stevens@infinitywork.com>

--- common_efs.tf
+++ common_efs.tf
@@ -1,54 +1,54 @@
-# resource "aws_efs_file_system" "prometheus" {
-#   tags = merge(local.tags, { Name = "prometheus_efs" })
-# }
+resource "aws_efs_file_system" "prometheus" {
+  tags = merge(local.tags, { Name = "prometheus_efs" })
+}
 
-# resource "aws_efs_mount_target" "prometheus" {
-#   count           = length(module.vpc.outputs.private_subnets[local.secondary_role_index])
-#   file_system_id  = aws_efs_file_system.prometheus.id
-#   subnet_id       = module.vpc.outputs.private_subnets[local.secondary_role_index][count.index]
-#   security_groups = [aws_security_group.prometheus_efs.id]
-# }
+resource "aws_efs_mount_target" "prometheus" {
+  count           = length(module.vpc.outputs.private_subnets[local.secondary_role_index])
+  file_system_id  = aws_efs_file_system.prometheus.id
+  subnet_id       = module.vpc.outputs.private_subnets[local.secondary_role_index][count.index]
+  security_groups = [aws_security_group.prometheus_efs.id]
+}
 
-# resource "aws_efs_access_point" "prometheus" {
-#   file_system_id = aws_efs_file_system.prometheus.id
+resource "aws_efs_access_point" "prometheus" {
+  file_system_id = aws_efs_file_system.prometheus.id
 
-#   root_directory {
-#     path = "/prometheus"
-#     creation_info {
-#       owner_gid   = 0
-#       owner_uid   = 0
-#       permissions = 600
-#     }
-#   }
+  root_directory {
+    path = "/prometheus"
+    creation_info {
+      owner_gid   = 0
+      owner_uid   = 0
+      permissions = 600
+    }
+  }
 
-#   posix_user {
-#     uid = 0
-#     gid = 0
-#   }
+  posix_user {
+    uid = 0
+    gid = 0
+  }
 
-#   tags = merge(local.tags, { Name = var.name })
-# }
+  tags = merge(local.tags, { Name = var.name })
+}
 
-# resource "aws_security_group" "prometheus_efs" {
-#   name        = "prometheus_efs"
-#   description = "Rules necesary for accessing EFS"
-#   vpc_id      = module.vpc.outputs.vpcs[local.secondary_role_index].id
-#   tags        = merge(local.tags, { Name = "prometheus_efs" })
+resource "aws_security_group" "prometheus_efs" {
+  name        = "prometheus_efs"
+  description = "Rules necesary for accessing EFS"
+  vpc_id      = module.vpc.outputs.vpcs[local.secondary_role_index].id
+  tags        = merge(local.tags, { Name = "prometheus_efs" })
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_security_group_rule" "efs_allow_ingress_prometheus" {
-#   description              = "Allow prometheus to access efs mount target"
-#   from_port                = 2049
-#   protocol                 = "tcp"
-#   security_group_id        = aws_security_group.prometheus_efs.id
-#   to_port                  = 2049
-#   type                     = "ingress"
-#   source_security_group_id = aws_security_group.prometheus.id
-# }
+resource "aws_security_group_rule" "efs_allow_ingress_prometheus" {
+  description              = "Allow prometheus to access efs mount target"
+  from_port                = 2049
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.prometheus_efs.id
+  to_port                  = 2049
+  type                     = "ingress"
+  source_security_group_id = aws_security_group.prometheus.id
+}
 
 resource "aws_efs_file_system" "outofband" {
   count = local.is_management_env ? 1 : 0
