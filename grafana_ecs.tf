75df3b824fec1b94d836f002705e2485ad4ee9e1
Levan K <37900502+TheDogfather17@users.noreply.github.com>
2020-12-24 15:31:29 +0000

    removed prometheus ecs and changed asg desired to 0 (#198)
    * removed prometheus ecs and changed asg desired to 0
    
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
    
    * revert max
    
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
    
    * remove all ecs
    
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
    
    * remove rest of ecs
    
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
    
    * remove pushgateway
    
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
    
    * remove outputs
    
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- grafana_ecs.tf
+++ grafana_ecs.tf
@@ -1,99 +1,99 @@
-resource "aws_ecs_task_definition" "grafana" {
-  count                    = local.is_management_env ? 1 : 0
-  family                   = "grafana"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "4096"
-  task_role_arn            = aws_iam_role.grafana[local.primary_role_index].arn
-  execution_role_arn       = local.is_management_env ? data.terraform_remote_state.management.outputs.ecs_task_execution_role.arn : data.terraform_remote_state.common.outputs.ecs_task_execution_role.arn
-  container_definitions    = "[${data.template_file.grafana_definition[local.primary_role_index].rendered}]"
-  tags                     = merge(local.tags, { Name = var.name })
-}
-
-data "template_file" "grafana_definition" {
-  count    = local.is_management_env ? 1 : 0
-  template = file("${path.module}/container_definition.tpl")
-  vars = {
-    name          = "grafana"
-    group_name    = "grafana"
-    cpu           = var.fargate_cpu
-    image_url     = data.terraform_remote_state.management.outputs.ecr_grafana_url
-    memory        = var.fargate_memory
-    user          = "grafana"
-    ports         = jsonencode([var.grafana_port])
-    ulimits       = jsonencode([])
-    log_group     = aws_cloudwatch_log_group.monitoring_metrics.name
-    region        = data.aws_region.current.name
-    config_bucket = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
-
-    mount_points = jsonencode([])
-
-    environment_variables = jsonencode([
-      {
-        "name" : "HTTP_PROXY",
-        "value" : "http://${aws_vpc_endpoint.internet_proxy[0].dns_entry[0].dns_name}:${var.internet_proxy_port}"
-      },
-      {
-        "name" : "HTTPS_PROXY",
-        "value" : "http://${aws_vpc_endpoint.internet_proxy[0].dns_entry[0].dns_name}:${var.internet_proxy_port}"
-      },
-      {
-        "name" : "NO_PROXY",
-        "value" : "127.0.0.1,s3.${var.region}.amazonaws.com,secretsmanager.${var.region}.amazonaws.com,${local.environment}.services.${var.parent_domain_name}"
-      },
-      {
-        "name" : "GRAFANA_CONFIG_CHANGE_DEPENDENCY",
-        "value" : "${md5(data.template_file.grafana[local.primary_role_index].rendered)}"
-      },
-      {
-        "name" : "SECRET_ID",
-        "value" : "/concourse/dataworks/monitoring"
-      }
-    ])
-  }
-}
-
-resource "aws_ecs_service" "grafana" {
-  count            = local.is_management_env ? 1 : 0
-  name             = "grafana"
-  cluster          = aws_ecs_cluster.metrics_ecs_cluster.id
-  task_definition  = aws_ecs_task_definition.grafana[local.primary_role_index].arn
-  platform_version = var.platform_version
-  desired_count    = 1
-  launch_type      = "FARGATE"
-
-  network_configuration {
-    security_groups = [aws_security_group.grafana[0].id, aws_security_group.monitoring_common[local.primary_role_index].id]
-    subnets         = module.vpc.outputs.private_subnets[local.primary_role_index]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.grafana[local.primary_role_index].arn
-    container_name   = "grafana"
-    container_port   = var.grafana_port
-  }
-
-  service_registries {
-    registry_arn   = aws_service_discovery_service.grafana[local.primary_role_index].arn
-    container_name = "grafana"
-  }
-
-  tags = merge(local.tags, { Name = var.name })
-}
-
-resource "aws_service_discovery_service" "grafana" {
-  count = local.is_management_env ? 1 : 0
-  name  = "grafana"
-
-  dns_config {
-    namespace_id = aws_service_discovery_private_dns_namespace.monitoring.id
-
-    dns_records {
-      ttl  = 10
-      type = "A"
-    }
-  }
-
-  tags = merge(local.tags, { Name = var.name })
-}
+//resource "aws_ecs_task_definition" "grafana" {
+//  count                    = local.is_management_env ? 1 : 0
+//  family                   = "grafana"
+//  network_mode             = "awsvpc"
+//  requires_compatibilities = ["FARGATE"]
+//  cpu                      = "512"
+//  memory                   = "4096"
+//  task_role_arn            = aws_iam_role.grafana[local.primary_role_index].arn
+//  execution_role_arn       = local.is_management_env ? data.terraform_remote_state.management.outputs.ecs_task_execution_role.arn : data.terraform_remote_state.common.outputs.ecs_task_execution_role.arn
+//  container_definitions    = "[${data.template_file.grafana_definition[local.primary_role_index].rendered}]"
+//  tags                     = merge(local.tags, { Name = var.name })
+//}
+//
+//data "template_file" "grafana_definition" {
+//  count    = local.is_management_env ? 1 : 0
+//  template = file("${path.module}/container_definition.tpl")
+//  vars = {
+//    name          = "grafana"
+//    group_name    = "grafana"
+//    cpu           = var.fargate_cpu
+//    image_url     = data.terraform_remote_state.management.outputs.ecr_grafana_url
+//    memory        = var.fargate_memory
+//    user          = "grafana"
+//    ports         = jsonencode([var.grafana_port])
+//    ulimits       = jsonencode([])
+//    log_group     = aws_cloudwatch_log_group.monitoring_metrics.name
+//    region        = data.aws_region.current.name
+//    config_bucket = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
+//
+//    mount_points = jsonencode([])
+//
+//    environment_variables = jsonencode([
+//      {
+//        "name" : "HTTP_PROXY",
+//        "value" : "http://${aws_vpc_endpoint.internet_proxy[0].dns_entry[0].dns_name}:${var.internet_proxy_port}"
+//      },
+//      {
+//        "name" : "HTTPS_PROXY",
+//        "value" : "http://${aws_vpc_endpoint.internet_proxy[0].dns_entry[0].dns_name}:${var.internet_proxy_port}"
+//      },
+//      {
+//        "name" : "NO_PROXY",
+//        "value" : "127.0.0.1,s3.${var.region}.amazonaws.com,secretsmanager.${var.region}.amazonaws.com,${local.environment}.services.${var.parent_domain_name}"
+//      },
+//      {
+//        "name" : "GRAFANA_CONFIG_CHANGE_DEPENDENCY",
+//        "value" : "${md5(data.template_file.grafana[local.primary_role_index].rendered)}"
+//      },
+//      {
+//        "name" : "SECRET_ID",
+//        "value" : "/concourse/dataworks/monitoring"
+//      }
+//    ])
+//  }
+//}
+//
+//resource "aws_ecs_service" "grafana" {
+//  count            = local.is_management_env ? 1 : 0
+//  name             = "grafana"
+//  cluster          = aws_ecs_cluster.metrics_ecs_cluster.id
+//  task_definition  = aws_ecs_task_definition.grafana[local.primary_role_index].arn
+//  platform_version = var.platform_version
+//  desired_count    = 1
+//  launch_type      = "FARGATE"
+//
+//  network_configuration {
+//    security_groups = [aws_security_group.grafana[0].id, aws_security_group.monitoring_common[local.primary_role_index].id]
+//    subnets         = module.vpc.outputs.private_subnets[local.primary_role_index]
+//  }
+//
+//  load_balancer {
+//    target_group_arn = aws_lb_target_group.grafana[local.primary_role_index].arn
+//    container_name   = "grafana"
+//    container_port   = var.grafana_port
+//  }
+//
+//  service_registries {
+//    registry_arn   = aws_service_discovery_service.grafana[local.primary_role_index].arn
+//    container_name = "grafana"
+//  }
+//
+//  tags = merge(local.tags, { Name = var.name })
+//}
+//
+//resource "aws_service_discovery_service" "grafana" {
+//  count = local.is_management_env ? 1 : 0
+//  name  = "grafana"
+//
+//  dns_config {
+//    namespace_id = aws_service_discovery_private_dns_namespace.monitoring.id
+//
+//    dns_records {
+//      ttl  = 10
+//      type = "A"
+//    }
+//  }
+//
+//  tags = merge(local.tags, { Name = var.name })
+//}
--- thanos_store_ecs.tf
+++ thanos_store_ecs.tf
@@ -1,81 +1,81 @@
-resource "aws_ecs_task_definition" "thanos_store" {
-  count                    = local.is_management_env ? 1 : 0
-  family                   = "thanos-store"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "1024"
-  memory                   = "4096"
-  task_role_arn            = aws_iam_role.thanos_store[local.primary_role_index].arn
-  execution_role_arn       = local.is_management_env ? data.terraform_remote_state.management.outputs.ecs_task_execution_role.arn : data.terraform_remote_state.common.outputs.ecs_task_execution_role.arn
-  container_definitions    = "[${data.template_file.thanos_store_definition[local.primary_role_index].rendered}]"
-  tags                     = merge(local.tags, { Name = var.name })
-}
-
-data "template_file" "thanos_store_definition" {
-  count    = local.is_management_env ? 1 : 0
-  template = file("${path.module}/container_definition.tpl")
-  vars = {
-    name          = "thanos-store"
-    group_name    = "thanos"
-    cpu           = var.store_cpu
-    image_url     = data.terraform_remote_state.management.outputs.ecr_thanos_url
-    memory        = var.fargate_memory
-    user          = "nobody"
-    ports         = jsonencode([var.thanos_port_grpc])
-    ulimits       = jsonencode([var.ulimits])
-    log_group     = aws_cloudwatch_log_group.monitoring_metrics.name
-    region        = data.aws_region.current.name
-    config_bucket = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
-
-    mount_points = jsonencode([])
-
-    environment_variables = jsonencode([
-      {
-        "name" : "THANOS_MODE",
-        "value" : "store"
-      },
-      {
-        "name" : "THANOS_STORE_CONFIG_CHANGE_DEPENDENCY",
-        "value" : "${md5(data.template_file.thanos_config.rendered)}"
-      }
-    ])
-  }
-}
-
-resource "aws_ecs_service" "thanos_store" {
-  count            = local.is_management_env ? 1 : 0
-  name             = "thanos-store"
-  cluster          = aws_ecs_cluster.metrics_ecs_cluster.id
-  task_definition  = aws_ecs_task_definition.thanos_store[local.primary_role_index].arn
-  platform_version = var.platform_version
-  desired_count    = 1
-  launch_type      = "FARGATE"
-
-  network_configuration {
-    security_groups = [aws_security_group.thanos_store[0].id, aws_security_group.monitoring_common[local.primary_role_index].id]
-    subnets         = module.vpc.outputs.private_subnets[local.primary_role_index]
-  }
-
-  service_registries {
-    registry_arn   = aws_service_discovery_service.thanos_store[local.primary_role_index].arn
-    container_name = "thanos-store"
-  }
-
-  tags = merge(local.tags, { Name = var.name })
-}
-
-resource "aws_service_discovery_service" "thanos_store" {
-  count = local.is_management_env ? 1 : 0
-  name  = "thanos-store"
-
-  dns_config {
-    namespace_id = aws_service_discovery_private_dns_namespace.monitoring.id
-
-    dns_records {
-      ttl  = 10
-      type = "A"
-    }
-  }
-
-  tags = merge(local.tags, { Name = var.name })
-}
+//resource "aws_ecs_task_definition" "thanos_store" {
+//  count                    = local.is_management_env ? 1 : 0
+//  family                   = "thanos-store"
+//  network_mode             = "awsvpc"
+//  requires_compatibilities = ["FARGATE"]
+//  cpu                      = "1024"
+//  memory                   = "4096"
+//  task_role_arn            = aws_iam_role.thanos_store[local.primary_role_index].arn
+//  execution_role_arn       = local.is_management_env ? data.terraform_remote_state.management.outputs.ecs_task_execution_role.arn : data.terraform_remote_state.common.outputs.ecs_task_execution_role.arn
+//  container_definitions    = "[${data.template_file.thanos_store_definition[local.primary_role_index].rendered}]"
+//  tags                     = merge(local.tags, { Name = var.name })
+//}
+//
+//data "template_file" "thanos_store_definition" {
+//  count    = local.is_management_env ? 1 : 0
+//  template = file("${path.module}/container_definition.tpl")
+//  vars = {
+//    name          = "thanos-store"
+//    group_name    = "thanos"
+//    cpu           = var.store_cpu
+//    image_url     = data.terraform_remote_state.management.outputs.ecr_thanos_url
+//    memory        = var.fargate_memory
+//    user          = "nobody"
+//    ports         = jsonencode([var.thanos_port_grpc])
+//    ulimits       = jsonencode([var.ulimits])
+//    log_group     = aws_cloudwatch_log_group.monitoring_metrics.name
+//    region        = data.aws_region.current.name
+//    config_bucket = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
+//
+//    mount_points = jsonencode([])
+//
+//    environment_variables = jsonencode([
+//      {
+//        "name" : "THANOS_MODE",
+//        "value" : "store"
+//      },
+//      {
+//        "name" : "THANOS_STORE_CONFIG_CHANGE_DEPENDENCY",
+//        "value" : "${md5(data.template_file.thanos_config.rendered)}"
+//      }
+//    ])
+//  }
+//}
+//
+//resource "aws_ecs_service" "thanos_store" {
+//  count            = local.is_management_env ? 1 : 0
+//  name             = "thanos-store"
+//  cluster          = aws_ecs_cluster.metrics_ecs_cluster.id
+//  task_definition  = aws_ecs_task_definition.thanos_store[local.primary_role_index].arn
+//  platform_version = var.platform_version
+//  desired_count    = 1
+//  launch_type      = "FARGATE"
+//
+//  network_configuration {
+//    security_groups = [aws_security_group.thanos_store[0].id, aws_security_group.monitoring_common[local.primary_role_index].id]
+//    subnets         = module.vpc.outputs.private_subnets[local.primary_role_index]
+//  }
+//
+//  service_registries {
+//    registry_arn   = aws_service_discovery_service.thanos_store[local.primary_role_index].arn
+//    container_name = "thanos-store"
+//  }
+//
+//  tags = merge(local.tags, { Name = var.name })
+//}
+//
+//resource "aws_service_discovery_service" "thanos_store" {
+//  count = local.is_management_env ? 1 : 0
+//  name  = "thanos-store"
+//
+//  dns_config {
+//    namespace_id = aws_service_discovery_private_dns_namespace.monitoring.id
+//
+//    dns_records {
+//      ttl  = 10
+//      type = "A"
+//    }
+//  }
+//
+//  tags = merge(local.tags, { Name = var.name })
+//}
