87a9a320f8b6c8b0e3d0ca054e87371d465bc81f
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-12-26 11:01:28 +0000

    back off cap change
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- cluster_ecs.tf
+++ cluster_ecs.tf
@@ -36,7 +36,7 @@ resource "aws_ecs_capacity_provider" "metrics_cluster" {
       maximum_scaling_step_size = 1000
       minimum_scaling_step_size = 1
       status                    = "ENABLED"
-      target_capacity           = 10
+      target_capacity           = 70
     }
   }
 
