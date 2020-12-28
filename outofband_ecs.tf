71d497aad5ab683a0e41e4de23c6140b0db2d22a
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-12-28 11:00:31 +0000

    rm sg rm platform ver
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- outofband_ecs.tf
+++ outofband_ecs.tf
@@ -85,13 +85,12 @@ data "template_file" "thanos_receiver_outofband_definition" {
 }
 
 resource "aws_ecs_service" "outofband" {
-  count            = local.is_management_env ? 1 : 0
-  name             = "outofband"
-  cluster          = aws_ecs_cluster.metrics_ecs_cluster.id
-  task_definition  = aws_ecs_task_definition.outofband[local.primary_role_index].arn
-  platform_version = var.platform_version
-  desired_count    = 1
-  launch_type      = "EC2"
+  count           = local.is_management_env ? 1 : 0
+  name            = "outofband"
+  cluster         = aws_ecs_cluster.metrics_ecs_cluster.id
+  task_definition = aws_ecs_task_definition.outofband[local.primary_role_index].arn
+  desired_count   = 1
+  launch_type     = "EC2"
 
   network_configuration {
     security_groups = [aws_security_group.outofband[local.primary_role_index].id, aws_security_group.monitoring_common[local.primary_role_index].id]
