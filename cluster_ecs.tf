6181ad5893beb875b160e11f627336be019bf917
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-01-04 12:17:58 +0000

    fmt
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- cluster_ecs.tf
+++ cluster_ecs.tf
@@ -40,9 +40,9 @@ resource "aws_ecs_capacity_provider" "metrics_cluster" {
     }
   }
 
-//  lifecycle {
-//    ignore_changes = all
-//  }
+  //  lifecycle {
+  //    ignore_changes = all
+  //  }
 
   tags = merge(
     local.tags,
