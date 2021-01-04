3b09c8eb82723a5a157d1031ace9a01a5ee145f3
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-01-04 12:05:04 +0000

    remove instance refresh
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- cluster_ecs.tf
+++ cluster_ecs.tf
@@ -72,12 +72,12 @@ resource "aws_autoscaling_group" "metrics_cluster" {
     create_before_destroy = true
   }
 
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 50
-    }
-  }
+  //  instance_refresh {
+  //    strategy = "Rolling"
+  //    preferences {
+  //      min_healthy_percentage = 50
+  //    }
+  //  }
 
   dynamic "tag" {
     for_each = local.metrics_ecs_asg_tags
