535db141b4d2628a977a8b25438806e46c5497b3
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-01-04 14:52:38 +0000

    set capacity provider disabled
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- cluster_ecs.tf
+++ cluster_ecs.tf
@@ -30,13 +30,13 @@ resource "aws_ecs_capacity_provider" "metrics_cluster" {
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.metrics_cluster.arn
-    managed_termination_protection = "ENABLED"
+    managed_termination_protection = "DISABLED"
 
     managed_scaling {
       maximum_scaling_step_size = 1000
       minimum_scaling_step_size = 1
       status                    = "ENABLED"
-      target_capacity           = 70
+      target_capacity           = 10
     }
   }
 
