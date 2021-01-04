508d6e08af4aeaa2bd7babd2c74d70d5d6795bbc
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-01-04 14:39:50 +0000

    put back to enabled
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- cluster_ecs.tf
+++ cluster_ecs.tf
@@ -30,7 +30,7 @@ resource "aws_ecs_capacity_provider" "metrics_cluster" {
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.metrics_cluster.arn
-    managed_termination_protection = "DISABLED"
+    managed_termination_protection = "ENABLED"
 
     managed_scaling {
       maximum_scaling_step_size = 1000
