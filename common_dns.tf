38d55120f1325a4139f40d10740ae08b0d621a47
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-01-08 10:23:18 +0000

    I'm willing to try anything
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- common_dns.tf
+++ common_dns.tf
@@ -202,16 +202,17 @@ resource "aws_route53_zone" "monitoring" {
 
 #this succeeds in creating authorisations from all envs -> mgmt/mgmt-dev monitoring-master
 resource "aws_route53_vpc_association_authorization" "monitoring" {
+  count   = local.is_management_env ? 0 : 1
   vpc_id  = local.is_management_env ? module.vpc.outputs.vpcs[0].id : data.terraform_remote_state.management_dmi.outputs.vpcs[0].id
   zone_id = aws_service_discovery_private_dns_namespace.monitoring.hosted_zone
 }
 
 #this succeeds in trying to create assocations from mgmt/mgmt-dev using authorisations that don't exist. e.g. aws_route53_vpc_association_authorization.monitoring[development].vpc_id
 resource "aws_route53_zone_association" "monitoring" {
-  for_each   = local.is_management_env ? local.dns_zone_ids[local.environment] : toset(local.environment)
+  for_each   = local.is_management_env ? local.dns_zone_ids[local.environment] : {}
   provider   = aws.management_dns
   vpc_id     = module.vpc.outputs.vpcs[0].id
-  zone_id    = lookup(local.dns_zone_ids, each.key, false) == false ? "" : each.value.hosted_zone
+  zone_id    = lookup(local.dns_zone_ids, each.key, false) == false ? "" : aws_service_discovery_private_dns_namespace.monitoring[each.key].hosted_zone[each.value]
   depends_on = [aws_route53_vpc_association_authorization.monitoring]
 }
 
