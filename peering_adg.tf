50d2a3d4c5a7b06b37d6e85f6eff359ceee35a9b
Levan K <37900502+TheDogfather17@users.noreply.github.com>
2021-01-14 16:57:16 +0000

    Jmx scraper (#228)
    * added scrape config for jmx
    
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
    
    * sg rules for jmx
    
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
    
    * added scrape config for yarn resources, and added relevant security group rules
    
    Signed-off-by: Mark Stevens <mark.stevens@infinityworks.com>
    
    * changed node exporter config
    
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
    
    * Update peering_adg.tf
    
    Co-authored-by: Mark Stevens <mark.stevens@infinityworks.com>

--- peering_adg.tf
+++ peering_adg.tf
@@ -63,3 +63,91 @@ resource "aws_security_group_rule" "prometheus_allow_egress_adg_node" {
   security_group_id        = aws_security_group.prometheus.id
   source_security_group_id = data.terraform_remote_state.aws_analytical_dataset_generation.outputs.adg_common_sg.id
 }
+
+resource "aws_security_group_rule" "prometheus_allow_egress_adg_jmx_namenode" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allow prometheus ${var.secondary} to access adg jmx metrics on namenode"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 7101
+  to_port                  = 7101
+  security_group_id        = aws_security_group.prometheus.id
+  source_security_group_id = data.terraform_remote_state.aws_analytical_dataset_generation.outputs.adg_common_sg.id
+}
+
+resource "aws_security_group_rule" "prometheus_allow_egress_adg_jmx_datanode" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allow prometheus ${var.secondary} to access adg jmx metrics on datanode"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 7103
+  to_port                  = 7103
+  security_group_id        = aws_security_group.prometheus.id
+  source_security_group_id = data.terraform_remote_state.aws_analytical_dataset_generation.outputs.adg_common_sg.id
+}
+
+resource "aws_security_group_rule" "adg_namenode_allow_ingress_prometheus" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allow prometheus ${var.secondary} to access adg namenode metrics"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 7101
+  to_port                  = 7101
+  security_group_id        = data.terraform_remote_state.aws_analytical_dataset_generation.outputs.adg_common_sg.id
+  source_security_group_id = aws_security_group.prometheus.id
+}
+
+resource "aws_security_group_rule" "adg_datanode_allow_ingress_prometheus" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allow prometheus ${var.secondary} to access adg datanode metrics"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 7103
+  to_port                  = 7103
+  security_group_id        = data.terraform_remote_state.aws_analytical_dataset_generation.outputs.adg_common_sg.id
+  source_security_group_id = aws_security_group.prometheus.id
+}
+
+resource "aws_security_group_rule" "adg_resoucre_manager_allow_ingress_prometheus" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allow prometheus ${var.secondary} to access adg yarn resource manager metrics"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 7105
+  to_port                  = 7105
+  security_group_id        = data.terraform_remote_state.aws_analytical_dataset_generation.outputs.adg_common_sg.id
+  source_security_group_id = aws_security_group.prometheus.id
+}
+
+resource "aws_security_group_rule" "adg_node_manager_allow_ingress_prometheus" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allow prometheus ${var.secondary} to access adg yarn node manager metrics"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 7107
+  to_port                  = 7107
+  security_group_id        = data.terraform_remote_state.aws_analytical_dataset_generation.outputs.adg_common_sg.id
+  source_security_group_id = aws_security_group.prometheus.id
+}
+
+resource "aws_security_group_rule" "prometheus_allow_egress_adg_resoucre_manager" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allow prometheus ${var.secondary} to access adg yarn resource manager metrics"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 7105
+  to_port                  = 7105
+  security_group_id        = aws_security_group.prometheus.id
+  source_security_group_id = data.terraform_remote_state.aws_analytical_dataset_generation.outputs.adg_common_sg.id
+}
+
+resource "aws_security_group_rule" "prometheus_allow_egress_adg_node_manager" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allow prometheus ${var.secondary} to access adg yarn resource manager metrics"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 7107
+  to_port                  = 7107
+  security_group_id        = aws_security_group.prometheus.id
+  source_security_group_id = data.terraform_remote_state.aws_analytical_dataset_generation.outputs.adg_common_sg.id
+}
