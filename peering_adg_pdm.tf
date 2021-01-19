d913561b4a185824903c89601f00dab1528f6f81
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-01-19 14:45:26 +0000

    added pdm prometheus ports and removed duplicate vpc peering
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- peering_adg_pdm.tf
+++ peering_adg_pdm.tf
@@ -1,23 +1,22 @@
-resource "aws_vpc_peering_connection" "adg" {
-  count       = local.is_management_env ? 0 : 1
-  peer_vpc_id = data.terraform_remote_state.aws_internal_compute.outputs.vpc.vpc.vpc.id
-  vpc_id      = module.vpc.outputs.vpcs[local.secondary_role_index].id
-  auto_accept = true
-  tags        = merge(local.tags, { Name = var.name })
-}
-
 resource "aws_route" "adg_prometheus_secondary" {
   count                     = local.is_management_env ? 0 : 1
   route_table_id            = data.terraform_remote_state.aws_internal_compute.outputs.route_table_ids.adg
   destination_cidr_block    = local.cidr_block[local.environment].mon-slave-vpc
-  vpc_peering_connection_id = aws_vpc_peering_connection.adg[0].id
+  vpc_peering_connection_id = aws_vpc_peering_connection.internal_compute[0].id
 }
 
 resource "aws_route" "prometheus_secondary_adg" {
   count                     = local.is_management_env ? 0 : local.zone_count
   route_table_id            = module.vpc.outputs.private_route_tables[local.secondary_role_index][count.index]
   destination_cidr_block    = local.cidr_block_internal_compute_vpc
-  vpc_peering_connection_id = aws_vpc_peering_connection.adg[0].id
+  vpc_peering_connection_id = aws_vpc_peering_connection.internal_compute[0].id
+}
+
+resource "aws_route" "pdm_prometheus_secondary" {
+  count                     = local.is_management_env ? 0 : 1
+  route_table_id            = data.terraform_remote_state.aws_internal_compute.outputs.route_table_ids.pdm
+  destination_cidr_block    = local.cidr_block[local.environment].mon-slave-vpc
+  vpc_peering_connection_id = aws_vpc_peering_connection.internal_compute[0].id
 }
 
 resource "aws_security_group_rule" "adg_allow_ingress_prometheus" {
@@ -260,4 +259,26 @@ resource "aws_security_group_rule" "pdm_namenode_allow_ingress_prometheus" {
   to_port                  = 7101
   security_group_id        = data.terraform_remote_state.aws_pdm_dataset_generation.outputs.pdm_common_sg.id
   source_security_group_id = aws_security_group.prometheus.id
+}
+
+resource "aws_security_group_rule" "pdm_allow_ingress_prometheus" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allow prometheus ${var.secondary} to access pdm metrics"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = var.prometheus_port
+  to_port                  = var.prometheus_port
+  security_group_id        = data.terraform_remote_state.aws_pdm_dataset_generation.outputs.pdm_common_sg.id
+  source_security_group_id = aws_security_group.prometheus.id
+}
+
+resource "aws_security_group_rule" "prometheus_allow_egress_pdm" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allow prometheus ${var.secondary} to access pdm metrics"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = var.prometheus_port
+  to_port                  = var.prometheus_port
+  security_group_id        = aws_security_group.prometheus.id
+  source_security_group_id = data.terraform_remote_state.aws_pdm_dataset_generation.outputs.pdm_common_sg.id
 }
