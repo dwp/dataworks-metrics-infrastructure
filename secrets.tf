0d44a768711e56be254a0707ba87dcbc51d372c8
Mark <mark.stevens@infinityworks.com>
2021-01-21 14:42:02 +0000

    fmt
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- secrets.tf
+++ secrets.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "monitoring_secrets" {
-  count = local.is_management_env ? 1 : 0
+  count       = local.is_management_env ? 1 : 0
   name        = "/concourse/dataworks/monitoring/credentials"
   description = "Secret paramaters used to store credentials to be accessed within Concourse"
   tags        = merge(local.tags, { Name = var.name })
