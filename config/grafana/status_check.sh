5df0d8fd5cc62c10f2eba33d1351f1b2bba69821
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-01-25 08:36:32 +0000

    get credentials
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- config/grafana/status_check.sh
+++ config/grafana/status_check.sh
@@ -1,11 +1,49 @@
 #!/bin/sh
-
+set -e
 http_code="000"
 
+# Checks Grafana service is available.
 while [[ $http_code != "200" ]]; do
     sleep 2
     curl -sL -w '%{http_code}' http://localhost:3000 -o /dev/null;
 done
+
+# If either of the AWS credentials variables were provided, validate them
+if [ -n "${AWS_ACCESS_KEY_ID}${AWS_SECRET_ACCESS_KEY}" ]; then
+    if [ -z "${AWS_ACCESS_KEY_ID}" -o -z "${AWS_SECRET_ACCESS_KEY}" ]; then
+        echo "ERROR: You must provide both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY variables if you want to use access key based authentication"
+        exit 1
+    else
+        echo "INFO: Using supplied access key for authentication"
+    fi
+    
+    # If either of the ASSUMEROLE variables were provided, validate them and configure a shared credentials fie
+    if [ -n "${AWS_ASSUMEROLE_ACCOUNT}${AWS_ASSUMEROLE_ROLE}" ]; then
+        if [ -z "${AWS_ASSUMEROLE_ACCOUNT}" -o -z "${AWS_ASSUMEROLE_ROLE}" ]; then
+            echo "ERROR: You must provide both the AWS_ASSUMEROLE_ACCOUNT and AWS_ASSUMEROLE_ROLE variables if you want to assume role"
+            exit 1
+        else
+            ASSUME_ROLE="arn:aws:iam::${AWS_ASSUMEROLE_ACCOUNT}:role/${AWS_ASSUMEROLE_ROLE}"
+            echo "INFO: Configuring AWS credentials for assuming role to ${ASSUME_ROLE}..."
+            mkdir ~/.aws
+      cat > ~/.aws/credentials << EOF
+[default]
+aws_access_key_id=${AWS_ACCESS_KEY_ID}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+
+[${AWS_ASSUMEROLE_ROLE}]
+role_arn=${ASSUME_ROLE}
+source_profile=default
+EOF
+            PROFILE_OPTION="--profile ${AWS_ASSUMEROLE_ROLE}"
+        fi
+    fi
+    if [ -n "${AWS_SESSION_TOKEN}" ]; then
+        sed -i -e "/aws_secret_access_key/a aws_session_token=${AWS_SESSION_TOKEN}" ~/.aws/credentials
+    fi
+else
+    echo "INFO: Using attached IAM roles/instance profiles to authenticate with S3 as no AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY have been provided"
+fi
 
 echo "INFO: Fetching grafana credentials from $SECRET_ID"
 GRAFANA_CREDENTIALS=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --query SecretString --output text | jq .grafana)
