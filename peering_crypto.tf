2177b5d751a516b4b9208f30b9069f731ccbe627
jackpopple <jack.popple@engineering.digital.dwp.gov.uk>
2021-01-28 11:28:45 +0000

    WIP - Added route from crypto to prometheus

--- peering_crypto.tf
+++ peering_crypto.tf
@@ -5,3 +5,10 @@ resource "aws_vpc_peering_connection" "crypto" {
   auto_accept = true
   tags        = merge(local.tags, { Name = var.name })
 }
+
+resource "aws_route" "crypto_prometheus" {
+  count                     = local.is_management_env ? 1 : 0
+  route_table_id            = data.terraform_remote_state.aws_crypto.outputs.dks_route_table.id
+  destination_cidr_block    = local.cidr_block[local.environment].mon-slave-vpc
+  vpc_peering_connection_id = aws_vpc_peering_connection.crypto[0].id
+}
