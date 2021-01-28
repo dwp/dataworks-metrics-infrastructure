d4e8c8cf68d4963f9c27468b4dbe28d40b59c504
jackpopple <jack.popple@engineering.digital.dwp.gov.uk>
2021-01-28 10:12:24 +0000

    WIP - Removed module.vpc from line 3 of peering_crypto.tf

--- peering_crypto.tf
+++ peering_crypto.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "crypto" {
   count       = local.is_management_env ? 1 : 0
-  peer_vpc_id = data.terraform_remote_state.aws_crypto.outputs.module.vpc.crypto_vpc.id
+  peer_vpc_id = data.terraform_remote_state.aws_crypto.outputs.crypto_vpc.id
   vpc_id      = module.vpc.outputs.vpcs[local.secondary_role_index].id
   auto_accept = true
   tags        = merge(local.tags, { Name = var.name })
