6f7396dc535fdd27ca957d1688d46c0dbac822b3
jackpopple <jack.popple@engineering.digital.dwp.gov.uk>
2021-01-28 09:12:46 +0000

    WIP - Created crypto peering resource Created remote_state data connection for crypto

---
+++ peering_crypto.tf
@@ -0,0 +1,7 @@
+resource "aws_vpc_peering_connection" "crypto" {
+  count       = local.is_management_env ? 1 : 0
+  peer_vpc_id = data.terraform_remote_state.aws_crypto.outputs.module.vpc.crypto_vpc.id
+  vpc_id      = module.vpc.outputs.vpcs[local.secondary_role_index].id
+  auto_accept = true
+  tags        = merge(local.tags, { Name = var.name })
+}
