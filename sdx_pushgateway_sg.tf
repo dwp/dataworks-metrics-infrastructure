a9dd6b40fea2ba3fcbfbc5eef02ea53cc5bb8575
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-02-15 17:34:22 +0000

    Remove unused groups

--- sdx_pushgateway_sg.tf
+++ sdx_pushgateway_sg.tf
@@ -32,28 +32,6 @@ resource "aws_security_group_rule" "allow_prometheus_ingress_sdx_pushgateway" {
   source_security_group_id = aws_security_group.prometheus.id
 }
 
-resource "aws_security_group_rule" "allow_sdx_ingress_sdx_pushgateway" {
-  count                    = local.is_management_env ? 0 : 1
-  description              = "Allows sdx to access sdx pushgateway"
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = var.pushgateway_port
-  to_port                  = var.pushgateway_port
-  security_group_id        = aws_security_group.sdx_pushgateway[0].id
-  source_security_group_id = data.terraform_remote_state.aws_sdx.outputs.sdx_common_sg.id
-}
-
-resource "aws_security_group_rule" "allow_sdx_egress_sdx_pushgateway" {
-  count                    = local.is_management_env ? 0 : 1
-  description              = "Allows sdx to access sdx pushgateway"
-  type                     = "egress"
-  protocol                 = "tcp"
-  from_port                = var.pushgateway_port
-  to_port                  = var.pushgateway_port
-  security_group_id        = data.terraform_remote_state.aws_sdx.outputs.sdx_common_sg.id
-  source_security_group_id = aws_security_group.sdx_pushgateway[0].id
-}
-
 resource "aws_security_group_rule" "allow_snapshot_sender_ingress_sdx_pushgateway" {
   count                    = local.is_management_env ? 0 : 1
   description              = "Allows Snapshot Sender to access sdx pushgateway"
