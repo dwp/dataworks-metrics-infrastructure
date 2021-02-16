316d77783e5e73ec49802ff781c2d4c61a886df1
steveburton4 <Steven.burton@hotmail.co.uk>
2021-02-16 09:13:21 +0000

    Fix the VPC the sdx SG is assigned to (#294)
    * Fix the VPC the sdx SG is assigned to
    
    * Fix output link

--- sdx_pushgateway_sg.tf
+++ sdx_pushgateway_sg.tf
@@ -2,7 +2,7 @@ resource "aws_security_group" "sdx_pushgateway" {
   count       = local.is_management_env ? 0 : 1
   name        = "sdx-pushgateway"
   description = "Rules necesary for pulling container image"
-  vpc_id      = data.terraform_remote_state.aws_internal_compute.outputs.vpc.vpc.vpc.id
+  vpc_id      = data.terraform_remote_state.aws_sdx.outputs.vpc.vpc.id
   tags        = merge(local.tags, { Name = "sdx-pushgateway" })
 
   lifecycle {
