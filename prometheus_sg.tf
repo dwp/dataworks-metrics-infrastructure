b0e9033a8e42291bf4fa56118533b9be367d0c4b
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-03-05 08:13:49 +0000

    Update prometheus_sg.tf
    Co-authored-by: steveburton4 <Steven.burton@hotmail.co.uk>

--- prometheus_sg.tf
+++ prometheus_sg.tf
@@ -65,7 +65,7 @@ resource "aws_security_group_rule" "allow_prometheus_egress_hbase_exporter" {
 
 resource "aws_security_group_rule" "allow_prometheus_egress_htme_pushgateway" {
   count                    = local.is_management_env ? 0 : 1
-  description              = "Allows prometheus to access SDX pushgateway"
+  description              = "Allows prometheus to access HTME pushgateway"
   type                     = "egress"
   protocol                 = "tcp"
   from_port                = var.pushgateway_port
