dac800b1f377ec4e14b5faab65c5170e85676541
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-03-05 07:45:43 +0000

    Fix SGs for failing Pushgateways, add Pushgateway Doc
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- prometheus_sg.tf
+++ prometheus_sg.tf
@@ -62,3 +62,25 @@ resource "aws_security_group_rule" "allow_prometheus_egress_hbase_exporter" {
   security_group_id        = aws_security_group.prometheus.id
   source_security_group_id = aws_security_group.hbase_exporter[0].id
 }
+
+resource "aws_security_group_rule" "allow_prometheus_egress_htme_pushgateway" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allows prometheus to access SDX pushgateway"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = var.pushgateway_port
+  to_port                  = var.pushgateway_port
+  security_group_id        = aws_security_group.prometheus.id
+  source_security_group_id = aws_security_group.htme_pushgateway[0].id
+}
+
+resource "aws_security_group_rule" "allow_prometheus_egress_ingest_pushgateway" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allows prometheus to access SDX pushgateway"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = var.pushgateway_port
+  to_port                  = var.pushgateway_port
+  security_group_id        = aws_security_group.prometheus.id
+  source_security_group_id = aws_security_group.ingest_pushgateway[0].id
+}
