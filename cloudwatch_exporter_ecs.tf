eba8a475832698ae1cc7428322248fb307412216
jackpopple <jack.popple@engineering.digital.dwp.gov.uk>
2021-03-17 14:15:58 +0000

    WIP - removed indexs from HTTP & HTTPS_PROXY env vars within the YACE container definition

--- cloudwatch_exporter_ecs.tf
+++ cloudwatch_exporter_ecs.tf
@@ -38,11 +38,11 @@ data "template_file" "cloudwatch_exporter_definition" {
       },
       {
         "name" : "HTTP_PROXY",
-        "value" : "http://${aws_vpc_endpoint.internet_proxy[0].dns_entry[0].dns_name}:${var.internet_proxy_port}"
+        "value" : "http://${aws_vpc_endpoint.internet_proxy.dns_entry.dns_name}:${var.internet_proxy_port}"
       },
       {
         "name" : "HTTPS_PROXY",
-        "value" : "http://${aws_vpc_endpoint.internet_proxy[0].dns_entry[0].dns_name}:${var.internet_proxy_port}"
+        "value" : "http://${aws_vpc_endpoint.internet_proxy.dns_entry.dns_name}:${var.internet_proxy_port}"
       },
     ])
   }
