dd65881e74311db6299faf43e5b224e76d32074f
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-03-25 14:56:40 +0000

    reduce sg's to correct ones, update ecs image to latest
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- peering_workflow_manager.tf
+++ peering_workflow_manager.tf
@@ -20,28 +20,6 @@ resource "aws_route" "prometheus_workflow_manager" {
   vpc_peering_connection_id = aws_vpc_peering_connection.workflow_manager[0].id
 }
 
-resource "aws_security_group_rule" "azkaban_executor_allow_ingress_prometheus" {
-  count                    = local.is_management_env ? 0 : 1
-  description              = "Allow prometheus ${var.secondary} to access azkaban_executor metrics"
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = 5556
-  to_port                  = 5556
-  security_group_id        = data.terraform_remote_state.aws-azkaban.outputs.azkaban_executor_sg.id
-  source_security_group_id = aws_security_group.prometheus.id
-}
-
-resource "aws_security_group_rule" "prometheus_allow_egress_azkaban_executor" {
-  count                    = local.is_management_env ? 0 : 1
-  description              = "Allow prometheus ${var.secondary} to access azkaban_executor metrics"
-  type                     = "egress"
-  protocol                 = "tcp"
-  from_port                = 5556
-  to_port                  = 5556
-  security_group_id        = aws_security_group.prometheus.id
-  source_security_group_id = data.terraform_remote_state.aws-azkaban.outputs.azkaban_executor_sg.id
-}
-
 resource "aws_security_group_rule" "azkaban_webserver_allow_ingress_prometheus" {
   count                    = local.is_management_env ? 0 : 1
   description              = "Allow prometheus ${var.secondary} to access azkaban_webserver metrics"
@@ -60,50 +38,6 @@ resource "aws_security_group_rule" "prometheus_allow_egress_azkaban_webserver" {
   protocol                 = "tcp"
   from_port                = 5556
   to_port                  = 5556
-  security_group_id        = aws_security_group.prometheus.id
-  source_security_group_id = data.terraform_remote_state.aws-azkaban.outputs.azkaban_webserver_sg.id
-}
-
-resource "aws_security_group_rule" "azkaban_executor_allow_ingress_prometheus_9998" {
-  count                    = local.is_management_env ? 0 : 1
-  description              = "Allow prometheus ${var.secondary} to access azkaban_executor metrics"
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = 9998
-  to_port                  = 9998
-  security_group_id        = data.terraform_remote_state.aws-azkaban.outputs.azkaban_executor_sg.id
-  source_security_group_id = aws_security_group.prometheus.id
-}
-
-resource "aws_security_group_rule" "prometheus_allow_egress_azkaban_executor_9998" {
-  count                    = local.is_management_env ? 0 : 1
-  description              = "Allow prometheus ${var.secondary} to access azkaban_executor metrics"
-  type                     = "egress"
-  protocol                 = "tcp"
-  from_port                = 9998
-  to_port                  = 9998
-  security_group_id        = aws_security_group.prometheus.id
-  source_security_group_id = data.terraform_remote_state.aws-azkaban.outputs.azkaban_executor_sg.id
-}
-
-resource "aws_security_group_rule" "azkaban_webserver_allow_ingress_prometheus_9998" {
-  count                    = local.is_management_env ? 0 : 1
-  description              = "Allow prometheus ${var.secondary} to access azkaban_webserver metrics"
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = 9998
-  to_port                  = 9998
-  security_group_id        = data.terraform_remote_state.aws-azkaban.outputs.azkaban_webserver_sg.id
-  source_security_group_id = aws_security_group.prometheus.id
-}
-
-resource "aws_security_group_rule" "prometheus_allow_egress_azkaban_webserver_9998" {
-  count                    = local.is_management_env ? 0 : 1
-  description              = "Allow prometheus ${var.secondary} to access azkaban_webserver metrics"
-  type                     = "egress"
-  protocol                 = "tcp"
-  from_port                = 9998
-  to_port                  = 9998
   security_group_id        = aws_security_group.prometheus.id
   source_security_group_id = data.terraform_remote_state.aws-azkaban.outputs.azkaban_webserver_sg.id
 }
