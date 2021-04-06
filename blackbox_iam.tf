f1d949c0d3aa0c19c325413a2a40118f4effdd2a
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-04-06 12:00:09 +0100

    Create Blackbox for checking Nifi status
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

---
+++ blackbox_iam.tf
@@ -0,0 +1,100 @@
+resource "aws_iam_role" "blackbox" {
+  count              = local.is_management_env ? 0 : 1
+  name               = "blackbox"
+  assume_role_policy = data.aws_iam_policy_document.blackbox_assume_role.json
+  tags               = merge(local.tags, { Name = "blackbox" })
+}
+
+data "aws_iam_policy_document" "blackbox_assume_role" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "blackbox_read_config_attachment" {
+  count      = local.is_management_env ? 0 : 1
+  role       = aws_iam_role.blackbox[local.primary_role_index].name
+  policy_arn = aws_iam_policy.blackbox_read_config[local.primary_role_index].arn
+}
+
+resource "aws_iam_policy" "blackbox_read_config" {
+  count       = local.is_management_env ? 0 : 1
+  name        = "BlackboxReadConfigPolicy"
+  description = "Allow Blackbox to read from config bucket"
+  policy      = data.aws_iam_policy_document.blackbox_read_config.json
+}
+
+data "aws_iam_policy_document" "blackbox_read_config" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "${local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.arn : data.terraform_remote_state.common.outputs.config_bucket.arn}",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.arn : data.terraform_remote_state.common.outputs.config_bucket.arn}/${var.name}/blackbox/*",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "kms:Decrypt",
+    ]
+
+    resources = [
+      "${local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.cmk_arn : data.terraform_remote_state.common.outputs.config_bucket_cmk.arn}",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "blackbox_ecs_exec" {
+  count      = local.is_management_env ? 0 : 1
+  role       = aws_iam_role.blackbox[local.primary_role_index].name
+  policy_arn = aws_iam_policy.blackbox_ecs_exec[local.primary_role_index].arn
+}
+
+resource "aws_iam_policy" "blackbox_ecs_exec" {
+  count       = local.is_management_env ? 0 : 1
+  name        = "BlackboxECSExecPolicy"
+  description = "Allow Blackbox container to exec from cli"
+  policy      = data.aws_iam_policy_document.blackbox_ecs_exec.json
+}
+
+data "aws_iam_policy_document" "blackbox_ecs_exec" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel"
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
---
+++ blackbox_sg.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "blackbox" {
+  count       = local.is_management_env ? 0 : 1
+  name        = "blackbox"
+  description = "Rules necesary for pulling container image and accessing other blackbox instances"
+  vpc_id      = data.terraform_remote_state.aws_sdx.outputs.vpc.vpc.id
+  tags        = merge(local.tags, { Name = "blackbox" })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "allow_blackbox_nifi_egress_https" {
+  count             = local.is_management_env ? 0 : 1
+  description       = "Allows ECS to pull container from S3"
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = var.https_port
+  to_port           = var.https_port
+  security_group_id = aws_security_group.blackbox[0].id
+  prefix_list_ids   = [data.terraform_remote_state.aws_internal_compute.outputs.vpc.vpc.prefix_list_ids.s3]
+}
+
+resource "aws_security_group_rule" "allow_prometheus_ingress_blackbox_nifi" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allows prometheus to access blackbox nifi"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 9115
+  to_port                  = 9115
+  security_group_id        = aws_security_group.blackbox[0].id
+  source_security_group_id = aws_security_group.prometheus.id
+}
