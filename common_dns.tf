aea3b95a02f80c3f2a83a3b5e36cb09edac704a7
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-04-08 15:05:53 +0100

    Sdx association (#322)
    * Add sdx services zones, add non-mgmt zone association
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Add sdx services zones, add non-mgmt zone association
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Add authorisation
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * should actually run in non-mgmt envs
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * count
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * silly dot
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * fmt
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * dummy on mgmt env
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Shift zone ids to secrets
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * Back to locals, like nothing happened
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * no vars no mo
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- common_dns.tf
+++ common_dns.tf
@@ -222,3 +222,17 @@ resource "aws_route53_zone_association" "monitoring" {
   zone_id    = each.value
   depends_on = [aws_route53_vpc_association_authorization.monitoring]
 }
+
+resource "aws_route53_vpc_association_authorization" "sdx_services" {
+  count   = local.is_management_env ? 0 : 1
+  vpc_id  = local.is_management_env ? null_resource.dummy.id : data.terraform_remote_state.management_dmi.outputs.vpcs[0].id
+  zone_id = aws_service_discovery_private_dns_namespace.sdx_services[0].hosted_zone
+}
+
+resource "aws_route53_zone_association" "sdx_services" {
+  count      = local.is_management_env ? 0 : 1
+  provider   = aws.management_zone
+  vpc_id     = local.is_management_env ? null_resource.dummy.id : data.terraform_remote_state.management_dmi.outputs.vpcs[0].id
+  zone_id    = local.is_management_env ? null_resource.dummy.id : local.sdx_dns_zone_ids[local.environment]
+  depends_on = [aws_route53_vpc_association_authorization.sdx_services]
+}
