0f4cc5633f9bc0315db74ffde49e1b67e0bf0802
Mark <mark.stevens@infinityworks.com>
2021-04-19 14:31:19 +0100

    outputting pdm pushgateway hostname
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,10 @@ output "adg_pushgateway_hostname" {
   value = local.is_management_env ? null_resource.dummy.id : "${aws_service_discovery_service.adg_pushgateway[0].name}.${aws_service_discovery_private_dns_namespace.adg_services[0].name}"
 }
 
+output "pdm_pushgateway_hostname" {
+  value = local.is_management_env ? null_resource.dummy.id : "${aws_service_discovery_service.pdm_pushgateway[0].name}.${aws_service_discovery_private_dns_namespace.pdm_services[0].name}"
+}
+
 output "sdx_pushgateway_security_group" {
   value = local.is_management_env ? null_resource.dummy.id : aws_security_group.sdx_pushgateway[0].id
 }
