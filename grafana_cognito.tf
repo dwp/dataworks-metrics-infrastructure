d7e944b3b938a8971f44f1c9f44456389ea5beae
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-05-26 07:59:49 +0100

    New file

---
+++ grafana_cognito.tf
@@ -0,0 +1,12 @@
+resource "aws_cognito_user_pool_client" "grafana" {
+  count                                = local.is_management_env ? 1 : 0
+  name                                 = "grafana"
+  user_pool_id                         = data.terraform_remote_state.dataworks_cognito.outputs.cognito.user_pool.id
+  callback_urls                        = ["https://${aws_route53_record.grafana_loadbalancer[0].fqdn}/login/generic_oauth"]
+  logout_urls                          = ["https://${aws_route53_record.grafana_loadbalancer[0].fqdn}"]
+  explicit_auth_flows                  = ["ALLOW_CUSTOM_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH"]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code", "implicit"]
+  allowed_oauth_scopes                 = ["phone", "aws.cognito.signin.user.admin", "email", "openid", "profile"]
+  supported_identity_providers         = ["COGNITO"]
+}
