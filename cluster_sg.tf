679e5d215101377fa8e3d59777c42576ff4df068
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-05-28 13:01:15 +0100

    Add rule

--- cluster_sg.tf
+++ cluster_sg.tf
@@ -52,6 +52,17 @@ resource "aws_security_group_rule" "allow_metrics_cluster_egress_pdm_exporter" {
   source_security_group_id = aws_security_group.pdm_exporter[0].id
 }
 
+resource "aws_security_group_rule" "allow_metrics_cluster_egress_pdm_pushgateway" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allows metrics cluster to access PDM pushgateway"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = var.pushgateway_port
+  to_port                  = var.pushgateway_port
+  security_group_id        = aws_security_group.metrics_cluster.id
+  source_security_group_id = data.terraform_remote_state.aws_internal_compute.outputs.vpce_security_groups.pdm_pushgateway_vpce_security_group.id
+}
+
 resource "aws_security_group_rule" "allow_metrics_cluster_egress_hbase_exporter" {
   count                    = local.is_management_env ? 0 : 1
   description              = "Allows metrics cluster to access Hbase exporter"
