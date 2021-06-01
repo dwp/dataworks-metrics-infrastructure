ae24d8c65fda6640972211529b8ade092e9bde03
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-06-01 13:17:05 +0100

    Re-add the services using the new discovery outputs

--- pdm_pushgateway_ecs.tf
+++ pdm_pushgateway_ecs.tf
@@ -38,26 +38,26 @@ data "template_file" "pdm_pushgateway_definition" {
   }
 }
 
-# resource "aws_ecs_service" "pdm_pushgateway" {
-#   count                              = local.is_management_env ? 0 : 1
-#   name                               = "pdm-pushgateway"
-#   cluster                            = aws_ecs_cluster.metrics_ecs_cluster.id
-#   task_definition                    = aws_ecs_task_definition.pdm_pushgateway[local.primary_role_index].arn
-#   platform_version                   = var.platform_version
-#   desired_count                      = 1
-#   launch_type                        = "FARGATE"
-#   deployment_minimum_healthy_percent = 100
-#   deployment_maximum_percent         = 200
+resource "aws_ecs_service" "pdm_pushgateway" {
+  count                              = local.is_management_env ? 0 : 1
+  name                               = "pdm-pushgateway"
+  cluster                            = aws_ecs_cluster.metrics_ecs_cluster.id
+  task_definition                    = aws_ecs_task_definition.pdm_pushgateway[local.primary_role_index].arn
+  platform_version                   = var.platform_version
+  desired_count                      = 1
+  launch_type                        = "FARGATE"
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
 
-#   network_configuration {
-#     security_groups = [data.terraform_remote_state.aws_internal_compute.outputs.vpce_security_groups.pdm_pushgateway_vpce_security_group.id]
-#     subnets         = data.terraform_remote_state.aws_internal_compute.outputs.pdm_subnet_new.ids
-#   }
+  network_configuration {
+    security_groups = [data.terraform_remote_state.aws_internal_compute.outputs.vpce_security_groups.pdm_pushgateway_vpce_security_group.id]
+    subnets         = data.terraform_remote_state.aws_internal_compute.outputs.pdm_subnet_new.ids
+  }
 
-#   service_registries {
-#     registry_arn   = aws_service_discovery_service.pdm_pushgateway[local.primary_role_index].arn
-#     container_name = "pdm-pushgateway"
-#   }
+  service_registries {
+    registry_arn   = data.terraform_remote_state.aws_pdm_dataset_generation.outputs.private_dns.pdm_service_discovery.arn
+    container_name = "pdm-pushgateway"
+  }
 
-#   tags = merge(local.tags, { Name = var.name })
-# }
+  tags = merge(local.tags, { Name = var.name })
+}
