befe93f72ef7e6398b848d5a9164d24a8609d4a1
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-06-02 09:37:20 +0100

    different subnets for mgt envs
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- cert_metrics_ecs.tf
+++ cert_metrics_ecs.tf
@@ -164,7 +164,7 @@ resource "aws_ecs_service" "cert_metrics" {
 
   network_configuration {
     security_groups = [aws_security_group.cert_metrics.id, aws_security_group.monitoring_common[local.secondary_role_index].id]
-    subnets         = module.vpc.outputs.private_subnets[local.secondary_role_index]
+    subnets         = local.is_management_env ? module.vpc.outputs.private_subnets[local.secondary_role_index] : module.vpc.outputs.private_subnets[local.primary_role_index]
   }
 
   service_registries {
