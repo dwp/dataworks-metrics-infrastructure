885680e5ab60274589ac5d45012024cf6609f089
Camilla S <43463939+camillascf@users.noreply.github.com>
2021-06-08 16:48:35 +0100

    put dashboard file in s3 (#392)

--- grafana_s3.tf
+++ grafana_s3.tf
@@ -85,6 +85,10 @@ data template_file "snapshot_sender_dashboard" {
   template = file("${path.module}/config/grafana/provisioning/dashboards/snapshot_sender.json")
 }
 
+data template_file "certificate_expiry_dashboard" {
+  template = file("${path.module}/config/grafana/provisioning/dashboards/certificate_expiry.json")
+}
+
 resource "aws_s3_bucket_object" "grafana" {
   count      = local.is_management_env ? 1 : 0
   bucket     = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
@@ -261,6 +265,15 @@ resource "aws_s3_bucket_object" "snapshot_sender_dashboard" {
   count      = local.is_management_env ? 1 : 0
   bucket     = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
   key        = "${var.name}/grafana/provisioning/dashboards/private/snapshot_sender.json"
+  content    = data.template_file.snapshot_sender_dashboard.rendered
+  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.cmk_arn : data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+  tags       = merge(local.tags, { Name = var.name })
+}
+
+resource "aws_s3_bucket_object" "certificate_expiry_dashboard" {
+  count      = local.is_management_env ? 1 : 0
+  bucket     = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "${var.name}/grafana/provisioning/dashboards/private/certificate_expiry.json"
   content    = data.template_file.snapshot_sender_dashboard.rendered
   kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.cmk_arn : data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
   tags       = merge(local.tags, { Name = var.name })
