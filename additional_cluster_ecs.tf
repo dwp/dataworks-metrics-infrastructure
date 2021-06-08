82cbd2f3f5736ed2c19f1532f8311648d31fa3c3
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-06-08 16:59:01 +0100

    primary vpc as thats where the cluster is
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- additional_cluster_ecs.tf
+++ additional_cluster_ecs.tf
@@ -223,7 +223,7 @@ resource "aws_launch_template" "additional_mgmt_metrics_cluster" {
     associate_public_ip_address = false
     delete_on_termination       = true
 
-    security_groups = [aws_security_group.mgmt_metrics_cluster[local.secondary_role_index].id, aws_security_group.monitoring_common[local.secondary_role_index].id]
+    security_groups = [aws_security_group.mgmt_metrics_cluster[local.primary_role_index].id, aws_security_group.monitoring_common[local.secondary_role_index].id]
   }
 
   user_data = base64encode(templatefile("userdata.tpl",
