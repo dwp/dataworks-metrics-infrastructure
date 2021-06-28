3a0bff4c71cd39ebaf5dbc1897e9b873f342b2c1
PoppleJIW <54989527+PoppleJIW@users.noreply.github.com>
2021-06-28 11:53:48 +0100

    Added sg rules for data-egress > sdx-pushgateway (#404)
    * Added sg rules for data-egress > sdx-pushgateway
    
    Signed-off-by: jackpopple <jack.popple@engineering.digital.dwp.gov.uk>
    
    * Removed .id
    
    Signed-off-by: jackpopple <jack.popple@engineering.digital.dwp.gov.uk>
    
    * switched security groups from ss to sdx pg
    
    Signed-off-by: Mark <mark.stevens@infinityworks.com>
    
    Co-authored-by: jackpopple <jack.popple@engineering.digital.dwp.gov.uk>
    Co-authored-by: Mark <mark.stevens@infinityworks.com>

--- sdx_pushgateway_sg.tf
+++ sdx_pushgateway_sg.tf
@@ -74,3 +74,25 @@ resource "aws_security_group_rule" "allow_prometheus_egress_sdx_pushgateway" {
   security_group_id        = aws_security_group.prometheus.id
   source_security_group_id = data.terraform_remote_state.aws_sdx.outputs.vpce_security_groups.sdx_pushgateway_vpce_security_group.id
 }
+
+resource "aws_security_group_rule" "allow_data_egress_ingress_sdx_pushgateway" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allows data egress to access sdx pushgateway"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = var.pushgateway_port
+  to_port                  = var.pushgateway_port
+  security_group_id        = data.terraform_remote_state.aws_sdx.outputs.vpce_security_groups.sdx_pushgateway_vpce_security_group.id
+  source_security_group_id = data.terraform_remote_state.dataworks-aws-data-egress.outputs.security_group.data_egress_server
+}
+
+resource "aws_security_group_rule" "allow_data_egress_egress_sdx_pushgateway" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allows data egress to access sdx pushgateway"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = var.pushgateway_port
+  to_port                  = var.pushgateway_port
+  security_group_id        = data.terraform_remote_state.dataworks-aws-data-egress.outputs.security_group.data_egress_server
+  source_security_group_id = data.terraform_remote_state.aws_sdx.outputs.vpce_security_groups.sdx_pushgateway_vpce_security_group.id
+}
