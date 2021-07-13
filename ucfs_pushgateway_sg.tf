6d37836c43ed58e4d34f8fb17b9c16a5b138c60b
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-07-13 12:03:21 +0100

    SG required for ucfs push gateway
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- ucfs_pushgateway_sg.tf
+++ ucfs_pushgateway_sg.tf
@@ -20,6 +20,17 @@ resource "aws_security_group_rule" "allow_prometheus_ingress_ucfs_claimant_api_p
   source_security_group_id = aws_security_group.prometheus.id
 }
 
+resource "aws_security_group_rule" "allow_prometheus_egress_ucfs_claimant_api_pushgateway" {
+  count                    = local.is_management_env ? 0 : 1
+  description              = "Allows prometheus to access ucfs claimant api pushgateway"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = var.pushgateway_port
+  to_port                  = var.pushgateway_port
+  security_group_id        = aws_security_group.prometheus.id
+  source_security_group_id = data.terraform_remote_state.ucfs-claimant.outputs.vpce_security_groups.ucfs_claimant_api_pushgateway.id
+}
+
 resource "aws_security_group_rule" "allow_ucfs_claimant_ingress_ucfs_claimant_api_pushgateway" {
   count             = local.is_management_env ? 0 : 1
   description       = "Allows ucfs claimant ireland vpc to access ucfs pushgateway"
