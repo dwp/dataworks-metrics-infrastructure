bd480b442607b700644a7c3d4bf39e1a9297e34f
rbryett <rory.bryett@gmail.com>
2021-12-10 13:34:15 +0000

    DW-7639 fix syntax

--- prometheus_sg.tf
+++ prometheus_sg.tf
@@ -46,8 +46,8 @@ resource "aws_security_group_rule" "allow_prometheus_egress_analytical_frontend_
   description              = "Allows prometheus to access Analytical Frontend Service"
   type                     = "egress"
   protocol                 = "tcp"
-  from_port                = data.terraform_remote_state.analytical-frontend-service.frontend_service.value.service_port
-  to_port                  = data.terraform_remote_state.analytical-frontend-service.frontend_service.value.service_port
+  from_port                = data.terraform_remote_state.analytical-frontend-service.frontend_service.service_port
+  to_port                  = data.terraform_remote_state.analytical-frontend-service.frontend_service.service_port
   security_group_id        = aws_security_group.prometheus.id
-  source_security_group_id = data.terraform_remote_state.analytical-frontend-service.ecs_tasks.id
+  source_security_group_id = data.terraform_remote_state.analytical-frontend-service.frontend_service.sg_id
 }
