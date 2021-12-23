fc2b13106698d134e7a693306a5cc47a5c1e05ee
rbryett <13303701+rbryett@users.noreply.github.com>
2021-12-23 10:39:11 +0000

    DW-7639 change scheme to https and do not verify as it is self-signed… (#451)
    * DW-7639 change scheme to https and do not verify as it is self-signed certificate we trust
    
    * DW-7639 add sgr to allow prometheus to scrape analytical frontend
    
    Co-authored-by: rorybryett <rory.bryett@dwp.gov.uk>

---
+++ analytical_frontend_sg.tf
@@ -0,0 +1,10 @@
+resource "aws_security_group_rule" "allow_prometheus_ingress_analytical_frontend" {
+  count                    = local.is_management_env ? 1 : 0
+  description              = "Allows prometheus to access analytical frontend service"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = data.terraform_remote_state.analytical-frontend-service.outputs.frontend_service.service_port
+  to_port                  = data.terraform_remote_state.analytical-frontend-service.outputs.frontend_service.service_port
+  security_group_id        = data.terraform_remote_state.analytical-frontend-service.outputs.frontend_service.sg_id
+  source_security_group_id = aws_security_group.prometheus.id
+}
