c303a38e91130e9faa15f8f65e89ed4c64d42c99
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2022-04-11 11:46:13 +0100

    adds htme_dns_zone related tf code to fix dns issue between htme & prometheus

--- common_dns.tf
+++ common_dns.tf
@@ -259,7 +259,19 @@ resource "aws_route53_zone_association" "clive_services" {
   zone_id    = local.is_management_env ? null_resource.dummy.id : local.clive_dns_zone_ids[local.environment]
   depends_on = [aws_route53_vpc_association_authorization.clive_services]
 }
+resource "aws_route53_vpc_association_authorization" "htme_services" {
+  count   = local.is_management_env ? 0 : 1
+  vpc_id  = local.is_management_env ? null_resource.dummy.id : module.vpc.outputs.vpcs[0].id
+  zone_id = data.terraform_remote_state.aws_internal_compute.outputs.private_dns.htme_service_discovery_dns.hosted_zone
+}
 
+resource "aws_route53_zone_association" "htme_services" {
+  count      = local.is_management_env ? 0 : 1
+  provider   = aws.non_management_zone
+  vpc_id     = local.is_management_env ? null_resource.dummy.id : module.vpc.outputs.vpcs[0].id
+  zone_id    = local.is_management_env ? null_resource.dummy.id : local.htme_dns_zone_ids[local.environment]
+  depends_on = [aws_route53_vpc_association_authorization.htme_services]
+}
 resource "aws_route53_zone_association" "uc_feature_services" {
   count      = local.is_management_env ? 0 : 1
   provider   = aws.non_management_zone
