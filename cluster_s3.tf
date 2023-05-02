9bc49e3d512915f66a1206edfb349fdf1f8b7e61
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-05-02 11:49:17 +0100

    fix: bucket key cmk

--- cluster_s3.tf
+++ cluster_s3.tf
@@ -6,7 +6,7 @@ resource "aws_s3_object" "metrics_logrotate_script" {
   bucket     = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
   key        = "component/metrics/metrics.logrotate"
   content    = data.local_file.metrics_logrotate_script.content
-  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket_cmk : data.terraform_remote_state.common.outputs.config_bucket_cmk
+  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket_cmk.arn : data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 
 
   tags = merge(
@@ -25,7 +25,7 @@ resource "aws_s3_object" "metrics_cloudwatch_script" {
   bucket     = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
   key        = "component/metrics/metrics-cloudwatch.sh"
   content    = data.local_file.metrics_cloudwatch_script.content
-  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket_cmk : data.terraform_remote_state.common.outputs.config_bucket_cmk
+  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket_cmk.arn : data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 
   tags = merge(
     local.tags,
@@ -43,7 +43,7 @@ resource "aws_s3_object" "metrics_logging_script" {
   bucket     = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
   key        = "component/metrics/logging.sh"
   content    = data.local_file.metrics_logging_script.content
-  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket_cmk : data.terraform_remote_state.common.outputs.config_bucket_cmk
+  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket_cmk.arn : data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 
   tags = merge(
     local.tags,
@@ -61,7 +61,7 @@ resource "aws_s3_object" "metrics_config_hcs_script" {
   bucket     = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket.id : data.terraform_remote_state.common.outputs.config_bucket.id
   key        = "component/metrics/config-hcs.sh"
   content    = data.local_file.metrics_config_hcs_script.content
-  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket_cmk : data.terraform_remote_state.common.outputs.config_bucket_cmk
+  kms_key_id = local.is_management_env ? data.terraform_remote_state.management.outputs.config_bucket_cmk.arn : data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 
   tags = merge(
     local.tags,
